d82a83075c1e92ede5ee7acce6bab596352b9710
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:47:34 +0700

    Initial commit

---
+++ tugas16.js
@@ -0,0 +1,7 @@
+const DeretAngka = () => {
+    var Y = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10)
+    var Z = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5)
+    console.log("Nilai Minimal : ", Y)
+    console.log("Nilai Maksimal : ", Z)
+}
+DeretAngka()
